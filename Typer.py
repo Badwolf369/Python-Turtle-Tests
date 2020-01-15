6e5ffcd51a0b8c50af30a8ad6765555a89c53150
Badwolf926 <sam.wilde369@gmail.com>
2020-01-15 18:55:26 +0000

    Add docstring to Typer.py

--- Typer.py
+++ Typer.py
@@ -2,6 +2,7 @@ from time import sleep
 from random import uniform
 
 def write(text):
+    """Prints given string one character at a time with slight delay"""
     for c in text:
         print(c, end='', flush=True)
         sleep(uniform(0.2, 0.1))
